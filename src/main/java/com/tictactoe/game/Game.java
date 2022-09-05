c7c65ebe40542d2cc0a115e3abc9a13b6194e376
Daniel Fintinariu <fintinariu.daniel@gmail.com>
2022-09-05 14:31:34 +0200

    Spring added

---
+++ src/main/java/com/tictactoe/game/Game.java
@@ -0,0 +1,15 @@
+package com.tictactoe.game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Game {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(Game.class, args);
+
+    }
+
+}
